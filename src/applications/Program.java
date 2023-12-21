a61f56b583743ff01c17e98f2e7b3495920e7e01
Iara Medeiros Barros <iaramedeirosbarros@gmail.com>
2023-12-21 13:29:31 -0300

    DaoFactory e SellerDaoJDBC

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -1,5 +1,7 @@
 package applications;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,7 @@ public class Program {
         Department d1 = new Department(1, "Books");
         Seller s1 = new Seller(1, "Bob Brown", "bobbrown@gmail.com", new Date(), 3000);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(d1);
         System.out.println(s1);
     }
