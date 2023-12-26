587c4b795dcc7ad817e77d6f267dc862c0a228a2
Iara Medeiros Barros <iaramedeirosbarros@gmail.com>
2023-12-26 15:03:37 -0300

    funcao insert

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -12,13 +12,14 @@ import java.util.List;
 public class Program {
     public static void main(String[] args) throws SQLException {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Department dep = new Department(2, null);
 
         System.out.println("=== TESTE 1 : seller FindById");
         Seller s1 = sellerDao.findById(2);
         System.out.println(s1);
 
         System.out.println("\n=== TESTE 2 : seller FindByDepartment");
-        List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+        List<Seller> list = sellerDao.findByDepartment(dep);
         for (Seller s: list) {
             System.out.println(s);
         }
@@ -28,5 +29,11 @@ public class Program {
         for (Seller s: list) {
             System.out.println(s);
         }
+
+        System.out.println("\n=== TESTE 4 : seller insertion");
+        Seller s = new Seller(null, "Greg Magenta","greg@gmail.com", new Date(), 2440, dep );
+        sellerDao.insert(s);
+        System.out.println("Inserted! New Id = " + s.getId());
+        System.out.println(sellerDao.findById(s.getId()));
     }
 }
