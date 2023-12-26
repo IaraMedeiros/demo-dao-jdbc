b2108cfe15d38cc099635f76c75abc9d4f56987e
Iara Medeiros Barros <iaramedeirosbarros@gmail.com>
2023-12-26 14:18:24 -0300

    função FindByDepartment

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -7,13 +7,21 @@ import model.entities.Seller;
 
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) throws SQLException {
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TESTE 1 : seller FindById");
-        SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller s1 = sellerDao.findById(2);
         System.out.println(s1);
+
+        System.out.println("\n=== TESTE 2 : seller FindByDepartment");
+        List<Seller> sellerByDep = sellerDao.findByDepartment(new Department(2, null));
+        for (Seller s: sellerByDep) {
+            System.out.println(s);
+            System.out.println("a");
+        }
     }
 }
